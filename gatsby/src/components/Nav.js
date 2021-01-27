d434a182d7a5f3b0cc9647b4e37b7b595b7628bc
tjuranek <thomas@juranek.com>
2021-01-26 20:36:28 -0600

    module 3

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,9 +1,53 @@
 import React from 'react';
-import { Link, navigate } from 'gatsby';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
+
+const NavStyles = styled.nav`
+	.logo {
+		transform: translateY(-25%);
+	}
+	margin-bottom: 3rem;
+	ul {
+		margin: 0;
+		padding: 0;
+		display: grid;
+		grid-template-columns: 1fr 1fr auto 1fr 1fr;
+		text-align: center;
+		list-style: none;
+		grid-gap: 20px;
+		align-items: center;
+		margin-top: -6rem;
+	}
+	li {
+		--rotate: -2deg;
+		transform: rotate(var(--rotate));
+		order: 1;
+		&:nth-child(1) {
+			--rotate: 1deg;
+		}
+		&:nth-child(2) {
+			--rotate: -2.5deg;
+		}
+		&:nth-child(4) {
+			--rotate: 2.5deg;
+		}
+		&:hover {
+			--rotate: 3deg;
+		}
+	}
+	a {
+		font-size: 3rem;
+		text-decoration: none;
+		&:hover {
+			color: var(--red);
+		}
+	}
+`;
 
 export default function Nav() {
 	return (
-		<nav>
+		<NavStyles>
 			<ul>
 				<li>
 					<Link to='/'>Home</Link>
@@ -12,7 +56,9 @@ export default function Nav() {
 					<Link to='/pizzas'>Pizza Menu</Link>
 				</li>
 				<li>
-					<Link to='/'>LOGO</Link>
+					<Link to='/'>
+						<Logo />
+					</Link>
 				</li>
 				<li>
 					<Link to='/slicemasters'>SliceMasters</Link>
@@ -21,6 +67,6 @@ export default function Nav() {
 					<Link to='/order'>Order</Link>
 				</li>
 			</ul>
-		</nav>
+		</NavStyles>
 	);
 }

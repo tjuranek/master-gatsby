cd0d1d5c18f31d5fa52b491fd990cb3f2af7ec88
tjuranek <thomas@juranek.com>
2021-02-23 21:30:36 -0600

    module 10

---
+++ gatsby/src/components/PizzaOrder.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import MenuItemStyles from '../styles/MenuItemStyles';
+import Img from 'gatsby-image';
+import formatMoney from '../utils/formatMoney';
+import calculatePizzaPrice from '../utils/calculatePizzaPrice';
+
+export default function PizzaOrder({ order, pizzas, removeFromOrder }) {
+	return (
+		<>
+			{order.map((singleOrder, index) => {
+				const pizza = pizzas.find(pizza => pizza.id == singleOrder.id);
+
+				return (
+					<MenuItemStyles key={index}>
+						<Img fluid={pizza.image.asset.fluid} />
+						<h2>{pizza.name}</h2>
+						<p>
+							{formatMoney(
+								calculatePizzaPrice(
+									pizza.price,
+									singleOrder.size
+								)
+							)}
+
+							<button
+								type='button'
+								className='remove'
+								title={`Remove ${singleOrder.size} ${pizza.name} from Order`}
+								onClick={() => removeFromOrder(index)}
+							>
+								&times;
+							</button>
+						</p>
+					</MenuItemStyles>
+				);
+			})}
+			<p>You have {order.length} items</p>
+		</>
+	);
+}
